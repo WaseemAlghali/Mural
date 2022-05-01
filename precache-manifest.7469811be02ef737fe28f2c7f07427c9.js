bc23b462779d38b074f3f20611e6286b7af5de15
Waseem Elghali <alghaliwaseem@gmail.com>
2022-05-01 22:00:58 +0300

    Updates

---
+++ precache-manifest.7469811be02ef737fe28f2c7f07427c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "69c16b4f8667b523256b",
+    "url": "/Mural/static/css/main.809694ea.chunk.css"
+  },
+  {
+    "revision": "69c16b4f8667b523256b",
+    "url": "/Mural/static/js/main.d71e221a.chunk.js"
+  },
+  {
+    "revision": "2f750441c0baf9ebacb5",
+    "url": "/Mural/static/js/runtime~main.f1d4e637.js"
+  },
+  {
+    "revision": "5c775f502f3dedc5fa4d",
+    "url": "/Mural/static/css/2.60fc7b3f.chunk.css"
+  },
+  {
+    "revision": "5c775f502f3dedc5fa4d",
+    "url": "/Mural/static/js/2.bc2e52a1.chunk.js"
+  },
+  {
+    "revision": "8dd7b1e66f1de49e659494d8a734d0fe",
+    "url": "/Mural/static/media/empty.8dd7b1e6.jpg"
+  },
+  {
+    "revision": "9bb09b539eca0763b8e66198d2ac9918",
+    "url": "/Mural/static/media/muralsong.9bb09b53.mp3"
+  },
+  {
+    "revision": "bfa32ecc1c43450c83f775f261beb20b",
+    "url": "/Mural/static/media/muralCompressed.bfa32ecc.gif"
+  },
+  {
+    "revision": "9a3881a617247b62ae74732d1156e679",
+    "url": "/Mural/index.html"
+  }
+];
